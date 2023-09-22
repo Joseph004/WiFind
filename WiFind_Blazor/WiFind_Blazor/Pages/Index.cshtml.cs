2f28e77e6bf7e8308b314fb4afe76752e253b2ee
jimmylindqvistt@gmail.com <jimmylindqvistt@gmail.com>
2023-09-22 20:18:53 +0200

    These files needs to be removed

---
+++ WiFind_Blazor/WiFind_Blazor/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiFind_Blazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
